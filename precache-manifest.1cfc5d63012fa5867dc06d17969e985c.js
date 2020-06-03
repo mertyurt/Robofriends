3809546e35156d2b0b6955c638479a0127e565c7
Mert Yurt <mert-yurt@hotmail.com>
2020-06-04 01:44:48 +0300

    Updates

---
+++ precache-manifest.1cfc5d63012fa5867dc06d17969e985c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d44e5619cf00b5563f1b42c43076367",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7e1482438e5de4b09364",
+    "url": "/Robofriends/static/css/main.e0b29d6c.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/Robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1482438e5de4b09364",
+    "url": "/Robofriends/static/js/main.234ba3ce.chunk.js"
+  },
+  {
+    "revision": "4eac723178dd1eab715f",
+    "url": "/Robofriends/static/js/runtime-main.33ca4b66.js"
+  }
+]);
