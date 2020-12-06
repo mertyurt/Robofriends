65cb7d28184b2b6cbc3a3fe8748581613c62071e
mertyurt <mert-yurt@hotmail.com>
2020-12-06 19:01:27 +0300

    hi

---
+++ src/robots.js
@@ -0,0 +1,37 @@
+export const robots = [
+    {
+        id: 1,
+        Name: "Bedo Mat1",
+        Email: "bedo.o.o@mat2.com"
+    },
+    {
+        id:2,
+        Name: "Gökche Gövaru",
+        Email: "gokce.ce.e@bombos.com"
+    },
+    {
+        id:3,
+        Name: "Enes KaraKedi",
+        Email: "karakedigireceginearamiza@blackcat.com"
+    },
+    {
+        id:4,
+        Name: "Yasin Yaso",
+        Email: "yasinyaskere@yasyasyas.com"
+    },
+    {
+        id:5,
+        Name: "Elif 3harflikılıç",
+        Email: "eloalo@decathlon.com"
+    },
+    {
+        id:6,
+        Name: "Büşra Bus",
+        Email: "busbus05@turizm.com"
+    },
+    {
+        id:7,
+        Name: "Metto Met",
+        Email: "metoo@youtoo?.com"
+    },
+]
